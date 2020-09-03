e0000438e62ea8bf6ca840027d96bb885f353ea0
arumakan1727 <aluminumcan7271@gmail.com>
2020-09-03 10:58:01 +0900

    Add union-find

---
+++ include/data-structure/union-find.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+/**
+ * @brief Union-Find (素集合データ構造)
+ * @note path-compression + union-by-size
+ */
+struct UnionFind {
+    mutable std::vector<int> p;
+
+    UnionFind() = default;
+    explicit UnionFind(size_t n) : p(n, -1) {}
+
+    bool unite(int x, int y) {
+        x = root(x), y = root(y);
+        if (x == y) return false;
+        if (p[y] < p[x]) std::swap(x, y);
+        p[x] += p[y];
+        p[y] = x;
+        return true;
+    }
+
+    int root(int x) const {
+        if (p[x] < 0) return x;
+        return p[x] = root(p[x]);
+    }
+
+    bool same(int x, int y) const {
+        return root(x) == root(y);
+    }
+
+    int size(int x) const {
+        return -p[root(x)];
+    }
+};
---
+++ test/DSL_1_A.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_A"
+#include <bits/stdc++.h>
+
+#include "../include/data-structure/union-find.hpp"
+#include "../include/template-parts/io-setup.hpp"
+#include "../include/template-parts/rep-macro.hpp"
+using namespace std;
+
+int main() {
+    ioSetup();
+    constexpr char LF = '\n';
+
+    int N, Q;
+    cin >> N >> Q;
+
+    UnionFind uf(N);
+    while (Q--) {
+        int com, x, y;
+        cin >> com >> x >> y;
+
+        if (com == 0) {
+            uf.unite(x, y);
+        } else {
+            cout << uf.same(x, y) << LF;
+        }
+    }
+
+    return 0;
+}
