2541e148ee4cf5581ceb2a45912a191c2dc51620
arumakan1727 <aluminumcan7271@gmail.com>
2020-09-09 18:08:47 +0900

    Refactor container-io.hpp

--- Util/IO/container-io.hpp
+++ Util/IO/container-io.hpp
@@ -4,17 +4,13 @@
 /**
  * @brief コンテナの入出力
  */
-template <class Container,
-          class Value = typename Container::value_type,
-          std::enable_if_t<!std::is_same<Container, std::string>::value, std::nullptr_t> = nullptr>
+template <class Container, class Value = typename Container::value_type, std::enable_if_t<!std::is_same_v<Container, std::string>, std::nullptr_t> = nullptr>
 std::istream& operator>>(std::istream& is, Container& v) {
     for (auto& e : v) is >> e;
     return is;
 }
 
-template <class Container,
-          class Value = typename Container::value_type,
-          std::enable_if_t<!std::is_same<Container, std::string>::value, std::nullptr_t> = nullptr>
+template <class Container, class Value = typename Container::value_type, std::enable_if_t<!std::is_same_v<Container, std::string>, std::nullptr_t> = nullptr>
 std::ostream& operator<<(std::ostream& os, const Container& v) {
     for (auto it = std::begin(v); it != std::end(v); ++it) os << " " + (it == std::begin(v)) << *it;
     return os;
