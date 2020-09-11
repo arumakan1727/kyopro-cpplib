8ba331d5094c82a3e60298f90e61689e81b5b9d8
arumakan1727 <aluminumcan7271@gmail.com>
2020-09-11 10:56:20 +0900

    Update utils

--- Util/int-infinity.hpp
+++ Util/int-infinity.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <cstdint>
+#include <limits>
+#include <type_traits>
 
 /**
  * @brief int-infinity (整数のデカイ値)
@@ -7,3 +9,13 @@
  */
 constexpr int32_t INF = 0x3f3f3f3f;
 constexpr int64_t LINF = 0x3f3f3f3f3f3f3f3fLL;
+
+template <class T, std::enable_if_t<std::is_integral_v<T> && sizeof(T) == 4, std::nullptr_t> = nullptr>
+constexpr T infinity() {
+    return INF;
+}
+
+template <class T, std::enable_if_t<std::is_integral_v<T> && sizeof(T) == 8, std::nullptr_t> = nullptr>
+constexpr T infinity() {
+    return LINF;
+}
