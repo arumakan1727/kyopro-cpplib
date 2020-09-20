f78778b0501e2e48bf4246c16b69e4eae5e5a923
arumakan1727 <aluminumcan7271@gmail.com>
2020-09-20 14:50:23 +0900

    Fix rep macro

--- Util/rep-macro.hpp
+++ Util/rep-macro.hpp
@@ -4,5 +4,5 @@
  * @brief rep()マクロ
  */
 #define rep(i, begin, end) for (std::make_signed_t<std::remove_cv_t<decltype(end)>> i = (begin), i##_end = (end); i < i##_end; ++i)
-#define repc(i, begin, last) for (std::make_signed_t<std::remove_cv_t<decltype(end)>> i = (begin), i##_last = (last); i <= i##_last; ++i)
-#define repr(i, begin, last) for (std::make_signed_t<std::remove_cv_t<decltype(end)>> i = (begin), i##_last = (last); i >= i##_last; --i)
+#define repc(i, begin, last) for (std::make_signed_t<std::remove_cv_t<decltype(last)>> i = (begin), i##_last = (last); i <= i##_last; ++i)
+#define repr(i, begin, last) for (std::make_signed_t<std::remove_cv_t<decltype(begin)>> i = (begin), i##_last = (last); i >= i##_last; --i)
