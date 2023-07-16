bebbbbfb5e2e438f767b35413f2fffcc481f3d05
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-16 16:43:32 +0900

    fix: ilog2

--- src/armkn/math/integer/ilog2.hpp
+++ src/armkn/math/integer/ilog2.hpp
@@ -3,12 +3,8 @@
 
 template <class Integer>
 inline constexpr unsigned ilog2(Integer n) {
-  assert(n >= 1);
-  if (n == 1) return 0;
+  if (n <= 1) return 0;
   unsigned k = 1;
-  while (n > 1) {
-    n >>= 1;
-    ++k;
-  }
-  return k;
+  while (n > 1) n >>= 1, ++k;
+  return k - 1;
 }
