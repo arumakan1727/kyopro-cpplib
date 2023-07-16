8cdcce7b0b7278ee0776c755bffccfb2b7d56a4c
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-16 16:37:08 +0900

    fix(prime_numbers): add overload that takes a sieve array in arg

--- src/armkn/math/integer/prime_numbers.hpp
+++ src/armkn/math/integer/prime_numbers.hpp
@@ -3,16 +3,22 @@
 #include "eratosthenes_sieve.hpp"
 #include "ilog2.hpp"
 
-/// n 以下の素数を昇順で返す
-auto prime_numbers(const unsigned n) -> std::vector<int> {
-  if (n < 2) return {};
-  auto p = eratosthenes_sieve(n);
+auto prime_numbers(const std::vector<bool>& sieve) -> std::vector<int> {
+  if (sieve.size() <= 2) return {};
+  auto n = sieve.size() - 1;
   std::vector<int> primes;
   primes.reserve((n / ilog2(n)) * 7 >> 2);
   if (n >= 2) primes.push_back(2);
   for (unsigned i = 3; i <= n; i += 2) {
-    if (p[i]) primes.push_back(i);
+    if (sieve[i]) primes.push_back(i);
   }
   primes.shrink_to_fit();
   return primes;
+}
+
+/// n 以下の素数を昇順で返す
+auto prime_numbers(const unsigned n) -> std::vector<int> {
+  if (n < 2) return {};
+  auto sieve = eratosthenes_sieve(n);
+  return prime_numbers(sieve);
 }
