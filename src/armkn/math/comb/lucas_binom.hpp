3038156914a2a0fa26454aec394ac923c4836da1
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-16 21:10:13 +0900

    bugfix+test: math/comb/lucas_binom.hpp

--- src/armkn/math/comb/lucas_binom.hpp
+++ src/armkn/math/comb/lucas_binom.hpp
@@ -1,34 +1,38 @@
 #pragma once
+#include <cassert>
 #include <cstdint>
 #include <vector>
+#include "../../util/alias/i32_i64.hpp"
+#include "../../util/alias/u32_u64.hpp"
+#include "../integer/is_prime.hpp"
 
-/// リュカの定理に基づいて nCk % MOD を計算する; MODが小さい場合に有効
+/// リュカの定理に基づいて nCk % MOD を計算する; MODが小さい素数の場合に有効
 ///   - 前計算: O(MOD^2)
 ///   - nCk:    O(log n)
 class LucasBinomial {
-  using u32 = uint32_t;
-  using i64 = int64_t;
-  using u64 = uint64_t;
-  std::vector<std::vector<uint32_t>> c;
+  std::vector<std::vector<u32>> c;
 
  public:
-  const uint32_t MOD;
+  const u32 MOD;
 
-  explicit LucasBinomial(u32 mod) : MOD(mod) {
+  explicit LucasBinomial(u32 mod) : c(mod, std::vector<u32>(mod)), MOD(mod) {
+    assert(mod < 100000);
+    assert(is_prime(mod));
     c[0][0] = 1;
     for (u32 i = 1; i < MOD; ++i) {
+      c[i].resize(MOD);
       c[i][0] = c[i][i] = 1;
       for (u32 j = 1; j < i; ++j) c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % MOD;
     }
   }
 
-  uint32_t C(const int64_t n_, const int64_t k_) const {
-    if (n_ < 0 || n_ < k_) return 0;
+  u32 C(const i64 n_, const i64 k_) const {
+    if (k_ < 0 || n_ < k_) return 0;
     u64 n(n_), k(k_), ret(1);
     while (n > 0) {
       (ret *= c[n % MOD][k % MOD]) %= MOD;
       n /= MOD, k /= MOD;
     }
-    return (uint32_t)ret;
+    return (u32)ret;
   }
 };
---
+++ tests/armkn/math/comb/lucas_binom.cpp
@@ -0,0 +1,81 @@
+#include <armkn/math/comb/lucas_binom.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("LucasBinomial", "[math][math/comb]") {
+  SECTION("MOD=97") {
+    constexpr uint32_t MOD = 127;
+    const auto f = LucasBinomial(MOD);
+    CHECK(f.MOD == 97);
+
+    CHECK(f.C(3, -1) == 0);
+    CHECK(f.C(3, 0) == 1);
+    CHECK(f.C(3, 1) == 3);
+    CHECK(f.C(3, 2) == 3);
+    CHECK(f.C(3, 3) == 1);
+    CHECK(f.C(3, 4) == 0);
+
+    CHECK(f.C(4, -1) == 0);
+    CHECK(f.C(4, 0) == 1);
+    CHECK(f.C(4, 1) == 4);
+    CHECK(f.C(4, 2) == 6);
+    CHECK(f.C(4, 3) == 4);
+    CHECK(f.C(4, 4) == 1);
+    CHECK(f.C(4, 5) == 0);
+
+    const uint64_t n = 100000;
+    CHECK(f.C(n, 0) == 1);
+    CHECK(f.C(n, 1) == n % MOD);
+    CHECK(f.C(n, 2) == n * (n - 1) / 2 % MOD);
+    CHECK(f.C(n, 3) == n * (n - 1) * (n - 2) / (3 * 2) % MOD);
+    CHECK(f.C(n, n - 1) == n % MOD);
+    CHECK(f.C(n, n) == 1);
+  }
+  SECTION("MOD=5") {
+    constexpr uint32_t MOD = 5;
+    const auto f = LucasBinomial(MOD);
+    CHECK(f.MOD == 5);
+
+    CHECK(f.C(0, -1) == 0);
+    CHECK(f.C(0, 0) == 1);
+    CHECK(f.C(0, 1) == 0);
+
+    CHECK(f.C(1, -1) == 0);
+    CHECK(f.C(1, 0) == 1);
+    CHECK(f.C(1, 1) == 1);
+    CHECK(f.C(1, 2) == 0);
+
+    CHECK(f.C(2, 0) == 1);
+    CHECK(f.C(2, 1) == 2);
+    CHECK(f.C(2, 2) == 1);
+
+    CHECK(f.C(3, 0) == 1);
+    CHECK(f.C(3, 1) == 3);
+    CHECK(f.C(3, 2) == 3);
+    CHECK(f.C(3, 3) == 1);
+
+    CHECK(f.C(4, 0) == 1);
+    CHECK(f.C(4, 1) == 4);
+    CHECK(f.C(4, 2) == 6 % MOD);
+    CHECK(f.C(4, 3) == 4);
+    CHECK(f.C(4, 4) == 1);
+
+    CHECK(f.C(5, 0) == 1);
+    CHECK(f.C(5, 1) == 0);
+    CHECK(f.C(5, 2) == 0);
+    CHECK(f.C(5, 3) == 0);
+    CHECK(f.C(5, 4) == 0);
+    CHECK(f.C(5, 5) == 1);
+
+    CHECK(f.C(26, 0) == 1);
+    CHECK(f.C(26, 1) == 26 % MOD);
+    CHECK(f.C(26, 2) == 0);  // 26 * 25 / 2 % MOD
+    CHECK(f.C(26, 3) == 0);
+    CHECK(f.C(26, 4) == 0);
+
+    CHECK(f.C(27, 0) == 1);
+    CHECK(f.C(27, 1) == 27 % MOD);
+    CHECK(f.C(27, 2) == 27 * 26 / 2 % MOD);
+    CHECK(f.C(27, 3) == 0);
+    CHECK(f.C(27, 4) == 0);
+  }
+}
