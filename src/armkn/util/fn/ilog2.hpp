6a35a220de5d0693d0bc93a924be8f81c1238fa3
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-17 14:39:12 +0900

    perf: impl `ilog2()` faster

--- src/armkn/util/fn/ilog2.hpp
+++ src/armkn/util/fn/ilog2.hpp
@@ -1,10 +1,16 @@
 #pragma once
-#include <cassert>
+#include <type_traits>
 
 template <class Integer>
-inline constexpr unsigned ilog2(Integer n) {
-  if (n <= 1) return 0;
-  unsigned k = 1;
-  while (n > 1) n >>= 1, ++k;
-  return k - 1;
+inline constexpr unsigned ilog2(Integer n_) {
+  if (n_ <= 1) return 0;
+  unsigned k = 0;
+  std::make_unsigned_t<Integer> n(n_), one{1};
+  if (sizeof(Integer) > 4 && (one << (k + 32)) <= n) k += 32;
+  if (sizeof(Integer) > 2 && (one << (k + 16)) <= n) k += 16;
+  if (sizeof(Integer) > 1 && (one << (k + 8)) <= n) k += 8;
+  if ((one << (k + 4)) <= n) k += 4;
+  if ((one << (k + 2)) <= n) k += 2;
+  if ((one << (k + 1)) <= n) k += 1;
+  return k;
 }
--- tests/armkn/util/fn/ilog2.cpp
+++ tests/armkn/util/fn/ilog2.cpp
@@ -1,5 +1,6 @@
 #include <armkn/util/fn/ilog2.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <limits>
 
 TEST_CASE("ilog2", "[util]") {
   CHECK(ilog2(1) == 0);
@@ -10,7 +11,15 @@ TEST_CASE("ilog2", "[util]") {
   CHECK(ilog2(6) == 2);
   CHECK(ilog2(7) == 2);
   CHECK(ilog2(8) == 3);
+  CHECK(ilog2(9) == 3);
   CHECK(ilog2((1u << 31) - 1) == 30);
   CHECK(ilog2((1u << 31) + 0) == 31);
   CHECK(ilog2((1u << 31) + 1) == 31);
+
+  CHECK(ilog2(std::numeric_limits<int8_t>::max()) == 6);
+  CHECK(ilog2(std::numeric_limits<uint8_t>::max()) == 7);
+  CHECK(ilog2(std::numeric_limits<int32_t>::max()) == 30);
+  CHECK(ilog2(std::numeric_limits<uint32_t>::max()) == 31);
+  CHECK(ilog2(std::numeric_limits<int64_t>::max()) == 62);
+  CHECK(ilog2(std::numeric_limits<uint64_t>::max()) == 63);
 }
