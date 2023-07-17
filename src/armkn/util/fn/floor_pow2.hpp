4e89a393e73638c936650aa48098dc9e375f82db
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-18 02:57:45 +0900

    perf(ilog2, floor_pow2): use __builtin_clz

--- src/armkn/util/fn/floor_pow2.hpp
+++ src/armkn/util/fn/floor_pow2.hpp
@@ -1,15 +1,11 @@
 #pragma once
-#include <type_traits>
 
 template <class Integer>
-inline constexpr Integer floor_pow2(Integer x_) {
-  if (x_ <= 0) return 0;
-  std::make_unsigned_t<Integer> x(x_), y{1};
-  if (sizeof(Integer) > 4 && (y << 32) <= x) y <<= 32;
-  if (sizeof(Integer) > 2 && (y << 16) <= x) y <<= 16;
-  if (sizeof(Integer) > 1 && (y << 8) <= x) y <<= 8;
-  if ((y << 4) <= x) y <<= 4;
-  if ((y << 2) <= x) y <<= 2;
-  if ((y << 1) <= x) y <<= 1;
-  return y;
+inline constexpr Integer floor_pow2(Integer x) {
+  if (x <= 0) return 0;
+  if constexpr (sizeof(Integer) > 4) {
+    return Integer(1ull << (63 - __builtin_clzll(x)));
+  } else {
+    return Integer(1u << (31 - __builtin_clz(x)));
+  }
 }
--- src/armkn/util/fn/ilog2.hpp
+++ src/armkn/util/fn/ilog2.hpp
@@ -1,16 +1,11 @@
 #pragma once
-#include <type_traits>
 
 template <class Integer>
-inline constexpr unsigned ilog2(Integer n_) {
-  if (n_ <= 1) return 0;
-  unsigned k = 0;
-  std::make_unsigned_t<Integer> n(n_), one{1};
-  if (sizeof(Integer) > 4 && (one << (k + 32)) <= n) k += 32;
-  if (sizeof(Integer) > 2 && (one << (k + 16)) <= n) k += 16;
-  if (sizeof(Integer) > 1 && (one << (k + 8)) <= n) k += 8;
-  if ((one << (k + 4)) <= n) k += 4;
-  if ((one << (k + 2)) <= n) k += 2;
-  if ((one << (k + 1)) <= n) k += 1;
-  return k;
+inline constexpr unsigned ilog2(Integer n) {
+  if (n <= 1) return 0;
+  if constexpr (sizeof(Integer) > 4) {
+    return 63 - __builtin_clzll(n);
+  } else {
+    return 31 - __builtin_clz(n);
+  }
 }
