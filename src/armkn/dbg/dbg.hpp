71f3fef436390a547c6f91ac7b59bd7e253c98ae
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-07-27 15:18:21 +0900

    feat(dbg.hpp): support debug printing of random accessible iterable

--- src/armkn/dbg/dbg.hpp
+++ src/armkn/dbg/dbg.hpp
@@ -1,9 +1,16 @@
 #pragma once
+#include <algorithm>
 #include <cctype>
+#include <cstddef>
 #include <cstring>
+#include <iomanip>
 #include <iostream>
+#include <iterator>
+#include <sstream>
 #include <string>
+#include <string_view>
 #include <tuple>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -16,6 +23,10 @@
 #define DBG_DISABLE_COLOR() ((void)0)
 
 #else
+
+#include "../util/type_traits/is_iterable.hpp"
+#include "../util/type_traits/is_random_accessible.hpp"
+#include "../util/type_traits/is_string_like.hpp"
 
 #define DBG(...)                  \
   ((void)::armkn::dbg::internal:: \
@@ -74,28 +85,104 @@ using namespace ::armkn::dbg::color;
 
 template <class T1, class T2>
 std::ostream& operator<<(std::ostream& o, const std::pair<T1, T2>& p) {
-  return o << '(' << p.first << ", " << p.second << ')';
+  return o << NOCOLOR << '(' << YELLOW << p.first << NOCOLOR << ", " << YELLOW << p.second
+           << NOCOLOR << ')';
 }
 
 template <class Tuple>
 void write_tuple_elements(std::ostream& o, Tuple&&, std::index_sequence<>) {
-  o << ')';
+  o << NOCOLOR << ')';
 }
 
 template <class Tuple, std::size_t Index, std::size_t... RestIndexes>
 void write_tuple_elements(std::ostream& o, Tuple&& t, std::index_sequence<Index, RestIndexes...>) {
   if constexpr (Index == 0) {
-    o << '(';
+    o << NOCOLOR << '(';
   } else {
-    o << ", ";
+    o << NOCOLOR << ", ";
   }
-  o << std::get<Index>(t);
+  o << YELLOW << std::get<Index>(t);
   write_tuple_elements(o, t, std::index_sequence<RestIndexes...>{});
 }
 
 template <class... T>
 std::ostream& operator<<(std::ostream& o, const std::tuple<T...>& t) {
   write_tuple_elements(o, t, std::index_sequence_for<T...>{});
+  return o;
+}
+
+template <class T>
+inline std::size_t nchars(const T& x) {
+  std::stringstream ss;
+  ss << x;
+  return ss.str().length();
+}
+
+void indent(std::ostream& o, int level) {
+  int w = level << 1;
+  while (w-- > 0) o.put(' ');
+}
+
+template <
+    class Iterable,
+    std::enable_if_t<armkn::is_random_accessible_v<Iterable>, std::nullptr_t> = nullptr>
+void write_iterable(std::ostream& o, const Iterable& xs, int nest = 0) {
+  using elem_type = std::decay_t<decltype(*std::begin(xs))>;
+  constexpr bool should_nest =
+      not armkn::is_string_like_v<elem_type> && armkn::is_iterable_v<elem_type>;
+
+  constexpr unsigned OMISSION_N = should_nest ? 30 : 120;
+  constexpr unsigned OMMSSION_TAIL_SHOW_N = should_nest ? 5 : 20;
+
+  const size_t n = std::end(xs) - std::begin(xs);
+  const size_t limitted_n = (n > OMISSION_N + OMMSSION_TAIL_SHOW_N) ? OMISSION_N : n;
+  const auto idx_nchars = (int)nchars(n - 1);
+
+  o << NOCOLOR << '{';
+
+  size_t i = 0;
+  const auto begin = std::begin(xs);
+
+  while (i < n) {
+    if (i == limitted_n) {
+      o << RED << "..., " << NOCOLOR;
+      i = 5 * ((n - OMMSSION_TAIL_SHOW_N) / 5);
+      continue;
+    }
+
+    if constexpr (should_nest) {
+      o << "\n";
+      indent(o, nest + 1);
+      o << DIM << WHITE << '[' << CYAN << std::setw(idx_nchars) << i << WHITE << "] " << NOCOLOR;
+      write_iterable(o, begin[i], nest + 1);
+    } else {
+      if (i >= 5 && i % 5 == 0) {
+        o << DIM << WHITE << '[' << CYAN << i << WHITE << "]" << NOCOLOR;
+      }
+      o << YELLOW << begin[i];
+    }
+
+    if (i + 1 < n) o << NOCOLOR << (should_nest ? "," : ", ");
+    ++i;
+  }
+
+  o << NOCOLOR;
+  if constexpr (should_nest) {
+    if (n) {
+      o.put('\n');
+      indent(o, nest);
+    }
+  }
+  o << '}';
+}
+
+template <
+    class Iterable,
+    std::enable_if_t<
+        not armkn::is_string_like_v<Iterable> && armkn::is_iterable_v<Iterable>,
+        std::nullptr_t> = nullptr>
+std::ostream& operator<<(std::ostream& o, const Iterable& xs) {
+  write_iterable(o, xs);
   return o;
 }
 
@@ -150,7 +237,7 @@ class DebugPrinter {
 
   template <class T>
   void put(T&& x) const {
-    // if string literal, show just value
+    // if arg is string literal, show just value
     if ((*itr)[0] == '"') {
       *out << '"' << GREEN << x << NOCOLOR << '"';
     } else {
@@ -162,12 +249,12 @@ class DebugPrinter {
     vector<string> res;
     char buf[128], *buf_end = buf;
     res.reserve(8);
-    unsigned paren = 0, brace = 0, bracket = 0;
+    unsigned paren = 0, brace = 0, bracket = 0, angle = 0;
 
     for (; *s != '\0'; ++s) {
       if (std::isspace(*s)) continue;
 
-      if (*s == ',' && paren == 0 && brace == 0 && bracket == 0) {
+      if (*s == ',' && paren == 0 && brace == 0 && bracket == 0 && angle == 0) {
         res.emplace_back(buf, buf_end);
         buf_end = buf;
         continue;
@@ -186,6 +273,10 @@ class DebugPrinter {
         ++bracket;
       else if (*s == ']')
         --bracket;
+      else if (*s == '<' && !std::isspace(s[-1]))
+        ++angle;
+      else if (*s == '>' && !std::isspace(s[-1]))
+        ++angle;
     }
 
     res.emplace_back(buf, buf_end);
--- tests/armkn/dbg/dbg.cpp
+++ tests/armkn/dbg/dbg.cpp
@@ -1,31 +1,61 @@
 #include <armkn/dbg/dbg.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/reporters/catch_reporter_event_listener.hpp>
+#include <catch2/reporters/catch_reporter_registrars.hpp>
 
 #include <array>
 #include <deque>
 #include <iostream>
 #include <list>
 #include <map>
+#include <regex>
 #include <set>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <utility>
 #include <vector>
 
+const auto RE_DBG_MSG_PREFIX = std::regex(R"([0-9][0-9]*:CATCH2_INTERNAL_TEST_0\(\): )");
+
 namespace {
 
-int func_with_arg(int a) {
-  return DBGV(a) * 100;
+std::string remove_dbg_prefix(const std::string& msg) {
+  return std::regex_replace(msg, RE_DBG_MSG_PREFIX, "");
+}
+
+void report_difference(const std::string_view got, const std::string_view want) {
+  if (got == want) return;
+  for (unsigned i = 0; i < std::min(got.size(), want.size()); ++i) {
+    if (got[i] != want[i]) {
+      std::clog << "Found difference: i=" << i << ", got[i]=`" << got[i] << "`, want[i]=`"
+                << want[i] << '`' << std::endl;
+
+      std::clog << "Near got: `";
+      for (int d = -5; d <= 5; ++d) {
+        if (i + d < got.size()) std::clog.put(got[i + d]);
+      }
+      std::clog << "`" << std::endl;
+
+      std::clog << "Near want: `";
+      for (int d = -5; d <= 5; ++d) {
+        if (i + d < want.size()) std::clog.put(want[i + d]);
+      }
+      std::clog << "`" << std::endl;
+
+      break;
+    }
+  }
 }
 
 template <class T>
 struct SomeStruct {
   T val;
 
-  std::pair<T, T> foo() const noexcept {
+  std::pair<T, T> foo(T x) const noexcept {
     DBG(this->val);
-    return {val, val};
+    return {val - x, val + x};
   }
 };
 
@@ -33,90 +63,62 @@ struct SomeStruct {
 
 TEST_CASE("dbg", "[dbg]") {
   using namespace std::string_literals;
-  using std::string;
-
-  // Check initial value
-  static bool is_first_test = true;
-  if (is_first_test) {
-    REQUIRE(::armkn::dbg::internal::out == &std::clog);
-
-    std::stringstream ss;
-    DBG_SET_OUTPUT(&ss);
-
-    using namespace ::armkn::dbg::color;
-    DBG(5);
-    CHECK(
-        ss.str() == ""s + MAGENTA + "47" + NOCOLOR + ':' + BLUE + "CATCH2_INTERNAL_TEST_0()" +
-                        NOCOLOR + ": " + CYAN + "5" + WHITE + DIM + "=" + NOCOLOR + YELLOW + "5" +
-                        NOCOLOR + "\n"
-    );
-    is_first_test = false;
-  }
-
   std::stringstream ss;
-  DBG_SET_OUTPUT(&ss);
 
-  SECTION("DBG_DISABLE_COLOR") {
-    ss.clear();
-    DBG_DISABLE_COLOR();
-
-    int a = 5, b = 3;
-    DBG(a + b);
-
-    DBG_DISABLE_COLOR();  // Check idempotence
-
-    DBG(a, b, a * (a / b));
-
-    [[maybe_unused]] int c = DBGV("hoge", a / b, a * b);
-    CHECK(
-        ss.str() ==
-        "64:CATCH2_INTERNAL_TEST_0(): a+b=8\n"                //
-        "68:CATCH2_INTERNAL_TEST_0(): a=5, b=3, a*(a/b)=5\n"  //
-        "70:CATCH2_INTERNAL_TEST_0(): \"hoge\", a/b=1, a*b=15\n"
-    );
-  }
-
-  SECTION("DBG_ENABLE_COLOR") {
+  SECTION("initial state") {
     using namespace ::armkn::dbg::color;
-    ss.clear();
-    DBG_ENABLE_COLOR();
-    DBG_ENABLE_COLOR();  // Check idempotence
+    static bool executed = false;
+    if (!executed) {
+      REQUIRE(::armkn::dbg::internal::out == &std::clog);
 
-    int a = 5;
-    DBG(a);
-    CHECK(
-        ss.str() == ""s + MAGENTA + "86" + NOCOLOR + ':' + BLUE + "CATCH2_INTERNAL_TEST_0()" +
-                        NOCOLOR + ": " + CYAN + "a" + WHITE + DIM + "=" + NOCOLOR + YELLOW + "5" +
-                        NOCOLOR + "\n"
-    );
+      DBG_SET_OUTPUT(&ss);
+      DBG(5);
+      CHECK(
+          ss.str() == ""s + MAGENTA + "75" + NOCOLOR + ':' + BLUE + "CATCH2_INTERNAL_TEST_0()" +
+                          NOCOLOR + ": " + CYAN + "5" + WHITE + DIM + "=" + NOCOLOR + YELLOW + "5" +
+                          NOCOLOR + "\n"
+      );
+      executed = true;
+    }
   }
 
   DBG_DISABLE_COLOR();
+  DBG_SET_OUTPUT(&ss);
 
-  SECTION("case of the func has argument") {
+  SECTION("case of complicated member func") {
     ss.clear();
-    func_with_arg(5);
-    CHECK(ss.str() == "19:func_with_arg(): a=5\n");
-  }
-
-  SECTION("case of member function in template class") {
-    ss.clear();
-    SomeStruct<int>{1727}.foo();
-    CHECK(ss.str() == "27:foo(): this->val=1727\n");
+    SomeStruct<int>{1727}.foo(5);
+    CHECK(ss.str().substr(2) == ":foo(): this->val=1727\n");
   }
 
   SECTION("case of the func is lambda func") {
     ss.clear();
     [](int x) { DBG(x + 1); }(777);
-    CHECK(ss.str() == "110:<lambda>(): x+1=778\n");
+    CHECK(ss.str().substr(2) == ":<lambda>(): x+1=778\n");
+  }
+
+  SECTION("primitive arithmetic expr") {
+    ss.clear();
+    int a = 5, b = 3;
+
+    DBG(a + b);
+    DBG(a, b, a * (a / b));
+    a = DBGV("hoge", a / b, a * b);
+
+    CHECK(
+        remove_dbg_prefix(ss.str()) ==
+        "a+b=8\n"                //
+        "a=5, b=3, a*(a/b)=5\n"  //
+        "\"hoge\", a/b=1, a*b=15\n"
+    );
+    CHECK(a == 15);
   }
 
   SECTION("DBGV") {
     ss.clear();
     auto x = DBGV(3.14, "str", false, -1 * 100);
-    CHECK(x == -100);
-
     auto y = DBGV(x);
+    CHECK(x == -100);
     CHECK(y == x);
   }
 
@@ -124,7 +126,7 @@ TEST_CASE("dbg", "[dbg]") {
     ss.clear();
     const char* s = "hello";
     DBG(std::pair{-1, s});
-    CHECK(ss.str() == "126:CATCH2_INTERNAL_TEST_0(): std::pair{-1,s}=(-1, hello)\n");
+    CHECK(remove_dbg_prefix(ss.str()) == "std::pair{-1,s}=(-1, hello)\n");
   }
 
   SECTION("tuple") {
@@ -132,79 +134,115 @@ TEST_CASE("dbg", "[dbg]") {
     const char* s = "hello";
     const auto t = std::tuple{s, std::pair{7, 4}, 3.14};
     DBG(std::tuple{-5}, t);
-    CHECK(
-        ss.str() == "134:CATCH2_INTERNAL_TEST_0(): std::tuple{-5}=(-5), t=(hello, (7, 4), 3.14)\n"
-    );
+    CHECK(remove_dbg_prefix(ss.str()) == "std::tuple{-5}=(-5), t=(hello, (7, 4), 3.14)\n");
   }
 
-  /* SECTION("vector<int>") { */
-  /*   ss.clear(); */
-  /*   std::vector<int> empty_vec{}, short_vec{3, 1, 4, 1}, */
-  /*       long_vec{0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22}, too_long_vec(203); */
-  /*   DBG(empty_vec); */
-  /*   DBG(short_vec); */
-  /*   DBG(long_vec); */
-  /*   DBG(too_long_vec); */
-  /*   CHECK( */
-  /*       ss.str() == */
-  /*       "121:CATCH2_INTERNAL_TEST_0(): empty_vec={}\n" */
-  /*       "122:CATCH2_INTERNAL_TEST_0(): short_vec={3, 1, 4, 1}\n" */
-  /**/
-  /*       "123:CATCH2_INTERNAL_TEST_0():"  // */
-  /*       " long_vec={[0]=0, 2, 4, 6, 8, [5]=10, 12, 14, 16, 18, [10]=20, 22}\n" */
-  /**/
-  /*       "124:CATCH2_INTERNAL_TEST_0(): \n"  // */
-  /*       " too_long_vec={"                   // */
-  /*       "  [  0]=0, 0, 0, 0, 0, [  5]=0, 0, 0, 0, 0, [ 10]=0, 0, 0, 0, 0, [ 15]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  [ 20]=0, 0, 0, 0, 0, [ 25]=0, 0, 0, 0, 0, [ 30]=0, 0, 0, 0, 0, [ 35]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  [ 40]=0, 0, 0, 0, 0, [ 45]=0, 0, 0, 0, 0, [ 50]=0, 0, 0, 0, 0, [ 55]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  [ 60]=0, 0, 0, 0, 0, [ 65]=0, 0, 0, 0, 0, [ 70]=0, 0, 0, 0, 0, [ 75]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  [ 80]=0, 0, 0, 0, 0, [ 85]=0, 0, 0, 0, 0, [ 90]=0, 0, 0, 0, 0, [ 95]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  [100]=0, 0, 0, 0, 0, [105]=0, 0, 0, 0, 0, [110]=0, 0, 0, 0, 0, [115]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  ...\n"  // */
-  /*       "  [180]=0, 0, 0, 0, 0, [185]=0, 0, 0, 0, 0, [190]=0, 0, 0, 0, 0, [195]=0, 0, 0, 0, 0,\n"
-   */
-  /*       "  [200]=0, 0, 0\n"  // */
-  /*       " }\n" */
-  /*   ); */
-  /* } */
-  /* SECTION("vector<vector<int>>") { */
-  /*   ss.clear(); */
-  /*   std::vector<std::vector<int>> small{ */
-  /*       {0, 1}, */
-  /*       {10, 20, 30}, */
-  /*   }; */
-  /*   std::vector<std::vector<int>> large{ */
-  /*       {3, 1, 4, 1, 5, 9, 2}, */
-  /*       {-1, 5, -30, 999}, */
-  /*       {}, */
-  /*       {-1, 0, 0, -0x3f3f3f3f, 1727}, */
-  /*   }; */
-  /*   DBG(small, large); */
-  /*   CHECK( */
-  /*       ss.str() == */
-  /*       "159:CATCH2_INTERNAL_TEST_0(): \n" */
-  /*       " small={\n"                                 // */
-  /*       "       [0] [1] [2]\n"                       // */
-  /*       "  [0] { 0,  1}\n"                           // */
-  /*       "  [1] {10, 20, 30}\n"                       // */
-  /*       " },\n"                                      // */
-  /*       " small={\n"                                 // */
-  /*       "       [0] [1]  [2]   [3]   [4] [5] [6]\n"  // */
-  /*       "  [0] { 3,  1,   4,    1,    5,  9,  2}\n"  // */
-  /*       "  [1] {-1,  5, -30,  999}\n"                // */
-  /*       "  [2] {},\n"                                // */
-  /*       "  [3] {-1,  0,   0, -INF, 1727}\n"          // */
-  /*       " },\n" */
-  /*   ); */
-  /* } */
+  SECTION("vector<int>") {
+    ss.clear();
+    std::vector<int> empty_vec{}, short_vec{3, 1, 4, 1},
+        long_vec{0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22}, too_long_vec(233);
+    DBG(empty_vec);
+    DBG(short_vec);
+    DBG(long_vec);
+    DBG(too_long_vec);
+    const std::string got = remove_dbg_prefix(ss.str());
+    constexpr std::string_view want =
+        ("empty_vec={}\n"
+         "short_vec={3, 1, 4, 1}\n"
+
+         "long_vec={0, 2, 4, 6, 8, [5]10, 12, 14, 16, 18, [10]20, 22}\n"
+
+         "too_long_vec={"
+         "0, 0, 0, 0, 0, [5]0, 0, 0, 0, 0, [10]0, 0, 0, 0, 0, [15]0, 0, 0, 0, 0, "
+         "[20]0, 0, 0, 0, 0, [25]0, 0, 0, 0, 0, [30]0, 0, 0, 0, 0, [35]0, 0, 0, 0, 0, "
+         "[40]0, 0, 0, 0, 0, [45]0, 0, 0, 0, 0, [50]0, 0, 0, 0, 0, [55]0, 0, 0, 0, 0, "
+         "[60]0, 0, 0, 0, 0, [65]0, 0, 0, 0, 0, [70]0, 0, 0, 0, 0, [75]0, 0, 0, 0, 0, "
+         "[80]0, 0, 0, 0, 0, [85]0, 0, 0, 0, 0, [90]0, 0, 0, 0, 0, [95]0, 0, 0, 0, 0, "
+         "[100]0, 0, 0, 0, 0, [105]0, 0, 0, 0, 0, [110]0, 0, 0, 0, 0, [115]0, 0, 0, 0, 0, "
+         "..., "
+         "[210]0, 0, 0, 0, 0, [215]0, 0, 0, 0, 0, [220]0, 0, 0, 0, 0, [225]0, 0, 0, 0, 0, "
+         "[230]0, 0, 0"
+         "}\n");
+    CHECK(got == want);
+    CHECK(got.length() == want.length());
+    report_difference(got, want);
+  }
+
+  SECTION("vector<deque<vector<int>>>") {
+    ss.clear();
+    std::array<std::deque<std::vector<int>>, 3> v{
+        std::deque{
+            std::vector{-1, 0xf, 1, 7, 2, 7, 998244353, 7},
+        },
+        {},
+        {
+            {},
+            {10, 20, 30},
+            {40, 50, 60, 70},
+        }};
+    DBG(v, v[0][0][0]);
+    const auto got = remove_dbg_prefix(ss.str());
+    const std::string_view want = R"(v={
+  [0] {
+    [0] {-1, 15, 1, 7, 2, [5]7, 998244353, 7}
+  },
+  [1] {},
+  [2] {
+    [0] {},
+    [1] {10, 20, 30},
+    [2] {40, 50, 60, 70}
+  }
+}, v[0][0][0]=-1
+)";
+    CHECK(got == want);
+    CHECK(got.length() == want.length());
+    report_difference(got, want);
+  }
+
   SECTION("deque") {}
   SECTION("set") {}
   SECTION("map") {}
 }
+
+namespace {
+
+class TestStartHook : public Catch::EventListenerBase {
+ public:
+  using Catch::EventListenerBase::EventListenerBase;
+
+  void testRunStarting(Catch::TestRunInfo const&) override {
+    REQUIRE(::armkn::dbg::internal::out == &std::clog);
+
+    using std::array;
+    using std::deque;
+    using std::pair;
+    using std::string;
+    using std::tuple;
+    using std::vector;
+
+    DBG("hello");
+    DBG(string("hello"));
+    DBG(tuple{-1 * 5, pair{0x3f3f3f3f, false}, "hello"});
+    DBG(deque<pair<int, int>>{{0, 3}, {1, 4}, {1, 5}, {9, 2}, {6, 5}, {3, 5}});
+    DBG(vector<int>{});
+    DBG(array<string, 3>{"hoge", "foo", "bar"});
+    DBG(vector<int>(302));
+
+    auto vec_3d = vector<vector<vector<int>>>{
+        {
+            {-1, 0x3f3f3f3f, 1, 7, 2, 7, 998244353, 7},
+        },
+        {},
+        {
+            {},
+            {10, 20, 30},
+            {40, 50, 60},
+        }};
+
+    DBG(vec_3d);
+  }
+};
+
+}  // namespace
+
+CATCH_REGISTER_LISTENER(TestStartHook)
