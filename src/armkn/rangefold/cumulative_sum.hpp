f5bb54776bf6c0be61a04d874f02cfee3874a482
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-08-08 13:10:11 +0900

    tweak

--- src/armkn/rangefold/cumulative_sum.hpp
+++ src/armkn/rangefold/cumulative_sum.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <algorithm>
 #include <cassert>
+#include <cstddef>
 #include <vector>
 
 template <class T>
@@ -18,20 +19,20 @@ struct CumulativeSum {
   }
 
   /// [0, i)
-  inline const T sum(int i) const {
-    assert(0 <= i && i < (int)data.size());
+  inline const T sum(size_t i) const {
+    assert(i < data.size());
     return data[i];
   }
 
   /// [l, r)
-  inline const T sum(int l, int r) const {
-    assert(0 <= l && l <= r && r < (int)data.size());
+  inline const T sum(size_t l, size_t r) const {
+    assert(l <= r && r < data.size());
     return data[r] - data[l];
   }
 
   /// `sum(l, r) >= v` となる最小の r を求める
-  inline std::size_t lower_bound(const T& v, int l) const {
-    assert(0 <= l && l < (int)data.size());
+  inline size_t lower_bound(const T& v, size_t l) const {
+    assert(l < data.size());
     return std::distance(data.begin(), std::lower_bound(data.begin(), data.end(), v + data[l]));
   }
 };
