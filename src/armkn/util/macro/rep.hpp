944b730fe3c3ec3010f51b725acabcacaa016b4a
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-08-14 14:47:43 +0900

    fix(macro/rep.hpp): use `std::make_signed_t` on REPR macro

--- src/armkn/util/macro/rep.hpp
+++ src/armkn/util/macro/rep.hpp
@@ -2,4 +2,4 @@
 
 #define REP(i, s, t) for (auto i = (s); (i) < (t); ++(i))
 #define REPC(i, s, t) for (auto i = (s); (i) <= (t); ++(i))
-#define REPR(i, s, t) for (auto i = (s); (i) >= (t); --(i))
+#define REPR(i, s, t) for (auto i = std::make_signed_t<decltype(s)>(s); (i) >= (t); --(i))
